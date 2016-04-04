758612d517f4b0d7f4ed60dfc2b86053debe7fb9
ihabritane <faycal.ihabritane@atos.net>
2016-04-04 16:20:14 +0100

    Ability to create Permission group using configuration #17

--- sm-shop/src/main/java/com/salesmanager/web/constants/ApplicationConstants.java
+++ sm-shop/src/main/java/com/salesmanager/web/constants/ApplicationConstants.java
@@ -5,5 +5,6 @@ public class ApplicationConstants {
 	public final static String POPULATE_TEST_DATA = "POPULATE_TEST_DATA";
 	public final static String TEST_DATA_LOADED = "TEST_DATA_LOADED";
 	public final static int MAX_DOWNLOAD_DAYS = 30;
+	public final static String PERMISSIONS_XML_FILE_LOCATION="/permissions.xml";
 
 }
--- sm-shop/src/main/java/com/salesmanager/web/init/data/InitializationLoader.java
+++ sm-shop/src/main/java/com/salesmanager/web/init/data/InitializationLoader.java
@@ -1,5 +1,10 @@
 package com.salesmanager.web.init.data;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.StringUtils;
@@ -13,8 +18,8 @@ import com.salesmanager.core.business.reference.init.service.InitializationDatab
 import com.salesmanager.core.business.system.model.SystemConfiguration;
 import com.salesmanager.core.business.system.service.SystemConfigurationService;
 import com.salesmanager.core.business.user.model.Group;
-import com.salesmanager.core.business.user.model.GroupType;
 import com.salesmanager.core.business.user.model.Permission;
+import com.salesmanager.core.business.user.model.PermissionList;
 import com.salesmanager.core.business.user.service.GroupService;
 import com.salesmanager.core.business.user.service.PermissionService;
 import com.salesmanager.core.constants.SystemConstants;
@@ -55,133 +60,32 @@ public class InitializationLoader {
 	@Autowired
 	private CoreConfiguration configuration;
 	
+	
+	@Autowired
+	private XMLConverter xmlConverter;
+	
 	@PostConstruct
 	public void init() {
 		
 		try {
 			
+			
 			if (initializationDatabase.isEmpty()) {
 				LOGGER.info(String.format("%s : Shopizer database is empty, populate it....", "sm-shop"));
 		
-				 initializationDatabase.populate("sm-shop");
-				
-				
-				
-				 //security groups and permissions
+				initializationDatabase.populate("sm-shop");
+				 
+				//security groups and permissions
 
-				  Group gsuperadmin = new Group("SUPERADMIN");
-				  gsuperadmin.setGroupType(GroupType.ADMIN);
-				  Group gadmin = new Group("ADMIN");
-				  gadmin.setGroupType(GroupType.ADMIN);
-				  Group gcatalogue = new Group("ADMIN_CATALOGUE");
-				  gcatalogue.setGroupType(GroupType.ADMIN);
-				  Group gstore = new Group("ADMIN_STORE");
-				  gstore.setGroupType(GroupType.ADMIN);
-				  Group gorder = new Group("ADMIN_ORDER");
-				  gorder.setGroupType(GroupType.ADMIN);
-				  Group gcontent = new Group("ADMIN_CONTENT");
-				  gcontent.setGroupType(GroupType.ADMIN);
-
-				  groupService.create(gsuperadmin);
-				  groupService.create(gadmin);
-				  groupService.create(gcatalogue);
-				  groupService.create(gstore);
-				  groupService.create(gorder);
-				  groupService.create(gcontent);
-				  
-				  Permission storeadmin = new Permission("STORE_ADMIN");//Administrator of the store
-				  storeadmin.getGroups().add(gsuperadmin);
-				  storeadmin.getGroups().add(gadmin);
-				  permissionService.create(storeadmin);
-				  
-				  Permission superadmin = new Permission("SUPERADMIN");
-				  superadmin.getGroups().add(gsuperadmin);
-				  permissionService.create(superadmin);
-				  
-				  Permission admin = new Permission("ADMIN");
-				  admin.getGroups().add(gsuperadmin);
-				  admin.getGroups().add(gadmin);
-				  permissionService.create(admin);
-				  
-				  Permission auth = new Permission("AUTH");//Authenticated
-				  auth.getGroups().add(gsuperadmin);
-				  auth.getGroups().add(gadmin);
-				  auth.getGroups().add(gcatalogue);
-				  auth.getGroups().add(gstore);
-				  auth.getGroups().add(gorder);
-				  permissionService.create(auth);
-
-				  
-				  Permission products = new Permission("PRODUCTS");
-				  products.getGroups().add(gsuperadmin);
-				  products.getGroups().add(gadmin);
-				  products.getGroups().add(gcatalogue);
-				  permissionService.create(products);
-
-				  
-				  Permission order = new Permission("ORDER");
-				  order.getGroups().add(gsuperadmin);
-				  order.getGroups().add(gorder);
-				  order.getGroups().add(gadmin);
-				  permissionService.create(order);
-				  
-				  Permission content = new Permission("CONTENT");
-				  content.getGroups().add(gsuperadmin);
-				  content.getGroups().add(gadmin);
-				  content.getGroups().add(gcontent);
-				  permissionService.create(content);
-				  
-				  
-				  
-				  Permission pstore = new Permission("STORE");
-				  pstore.getGroups().add(gsuperadmin);
-				  pstore.getGroups().add(gstore);
-				  pstore.getGroups().add(gadmin);
-				  permissionService.create(pstore);
-				  
-				  Permission tax = new Permission("TAX");
-				  tax.getGroups().add(gsuperadmin);
-				  tax.getGroups().add(gstore);
-				  tax.getGroups().add(gadmin);
-				  permissionService.create(tax);
-				  
-				  
-				  Permission payment = new Permission("PAYMENT");
-				  payment.getGroups().add(gsuperadmin);
-				  payment.getGroups().add(gstore);
-				  payment.getGroups().add(gadmin);
-				  permissionService.create(payment);
-				  
-				  Permission customer = new Permission("CUSTOMER");
-				  customer.getGroups().add(gsuperadmin);
-				  customer.getGroups().add(gstore);
-				  customer.getGroups().add(gadmin);
-				  permissionService.create(customer);
-				  
-				  
-				  Permission shipping = new Permission("SHIPPING");
-				  shipping.getGroups().add(gsuperadmin);
-				  shipping.getGroups().add(gadmin);
-				  shipping.getGroups().add(gstore);
-				  
-				  permissionService.create(shipping);
+				Map<String,Group>  groupAlreadyCreated=new HashMap<>();
+				PermissionList permissionList = (PermissionList) xmlConverter.doUnMarshaling(ApplicationConstants.PERMISSIONS_XML_FILE_LOCATION);
+				if (Optional.ofNullable(permissionList).isPresent()) {
+						savePermissionWithGroup(groupAlreadyCreated,permissionList);
+				}
 				
-				
-				
-				  userDetailsService.createDefaultAdmin();
+				userDetailsService.createDefaultAdmin();
 				  
-				  
-				  //load customer groups and permissions
-				  Group gcustomer = new Group("CUSTOMER");
-				  gcustomer.setGroupType(GroupType.CUSTOMER);
-				  
-				  groupService.create(gcustomer);
-				  
-				  Permission gcustomerpermission = new Permission("AUTH_CUSTOMER");
-				  gcustomerpermission.getGroups().add(gcustomer);
-				  permissionService.create(gcustomerpermission);
-
-				  loadData();
+				loadData();
 
 			}
 			
@@ -189,8 +93,34 @@ public class InitializationLoader {
 			LOGGER.error("Error in the init method",e);
 		}
 		
+	}
 
+	private void savePermissionWithGroup(Map<String, Group> groupAlreadyCreated,PermissionList permissionList) throws ServiceException {
 		
+		List<Permission> permissions = permissionList.getPermissions();
+		
+		for (Permission permission : permissions) {
+			
+			for (int index = 0; index < permission.getGroups().size(); index++) {
+				
+				Group group = permission.getGroups().get(index);
+			
+				if (!groupAlreadyCreated.containsKey(group.getGroupName())) {
+					
+					groupService.create(group);
+					
+					groupAlreadyCreated.put(group.getGroupName(), group);
+					
+				} else {
+					
+					permission.getGroups().remove(index);
+					
+					permission.getGroups().add(index,groupAlreadyCreated.get(group.getGroupName()));
+				}
+			}
+			
+			permissionService.create(permission);
+		}
 	}
 	
 	private void loadData() throws ServiceException {
