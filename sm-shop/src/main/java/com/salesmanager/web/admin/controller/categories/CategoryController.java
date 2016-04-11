571a6c201bd590d17a9e8368cc8accdbfd1eaef1
umeshawasthi@gmail.com <umeshawasthi@gmail.com>
2016-04-10 23:11:55 -0700

    added comment

--- sm-shop/src/main/java/com/salesmanager/web/admin/controller/categories/CategoryController.java
+++ sm-shop/src/main/java/com/salesmanager/web/admin/controller/categories/CategoryController.java
@@ -42,6 +42,7 @@ public class CategoryController {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CategoryController.class);
 	
+	//test
 	@Autowired
 	LanguageService languageService;
 	
