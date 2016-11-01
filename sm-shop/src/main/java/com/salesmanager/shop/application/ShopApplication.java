c9bcf3bb6992dba9f463e699f6105d488b0f89d2
Samson <c.samson@cgi.com>
2016-11-01 15:23:14 -0400

    Fixed component-scan for core

--- sm-shop/src/main/java/com/salesmanager/shop/application/ShopApplication.java
+++ sm-shop/src/main/java/com/salesmanager/shop/application/ShopApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.salesmanager.shop.*")
 public class ShopApplication {
 	
     public static void main(String[] args) {
