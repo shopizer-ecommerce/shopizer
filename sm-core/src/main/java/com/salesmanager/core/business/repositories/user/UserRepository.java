25f2939b657cec642fa01998f266c001b60d2ef1
Samson <c.samson@cgi.com>
2016-11-15 15:31:39 -0500

    Security issues

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/user/UserRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/user/UserRepository.java
@@ -9,15 +9,15 @@ import com.salesmanager.core.model.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	@Query("select u from User as u inner join fetch u.groups ug inner join fetch u.merchantStore um inner join fetch u.defaultLanguage ul where u.adminName = ?1")
+	@Query("select u from User as u inner join fetch u.groups ug inner join fetch u.merchantStore um left join fetch u.defaultLanguage ul where u.adminName = ?1")
 	User findByUserName(String userName);
 	
-	@Query("select u from User as u inner join fetch u.groups ug inner join fetch u.merchantStore um inner join fetch u.defaultLanguage ul where u.id = ?1")
+	@Query("select u from User as u inner join fetch u.groups ug inner join fetch u.merchantStore um left join fetch u.defaultLanguage ul where u.id = ?1")
 	User findById(Long id);
 	
-	@Query("select u from User as u inner join fetch u.groups ug inner join fetch u.merchantStore um inner join fetch u.defaultLanguage ul order by u.id")
+	@Query("select u from User as u inner join fetch u.groups ug inner join fetch u.merchantStore um left join fetch u.defaultLanguage ul order by u.id")
 	List<User> findAll();
 	
-	@Query("select distinct u from User as u inner join fetch u.groups ug inner join fetch u.merchantStore um inner join fetch u.defaultLanguage ul where um.id = ?1 order by u.id")
+	@Query("select distinct u from User as u inner join fetch u.groups ug inner join fetch u.merchantStore um left join fetch u.defaultLanguage ul where um.id = ?1 order by u.id")
 	List<User> findByStore(Integer storeId);
 }
