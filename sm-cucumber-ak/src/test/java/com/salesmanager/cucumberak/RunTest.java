ab89d387416f642862c4b0af74c42920011b9555
Anna Kry <Anna Kry>
2017-05-24 14:51:37 +0200

    added Cucumber tests

---
+++ sm-cucumber-ak/src/test/java/com/salesmanager/cucumberak/RunTest.java
@@ -0,0 +1,15 @@
+package com.salesmanager.cucumberak;
+ 
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/com/salesmanager/cucumberak/"
+		,glue={"classpath:"}
+		)
+ 
+public class RunTest {
+ 
+}
