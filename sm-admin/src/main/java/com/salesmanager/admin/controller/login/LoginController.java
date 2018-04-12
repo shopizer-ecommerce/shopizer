1d792f83b8375bf2ef7f3f86157b4ba4dd606bd2
Samson <c.samson@cgi.com>
2018-04-12 12:50:18 -0400

    login screen

--- sm-admin/src/main/java/com/salesmanager/admin/controller/login/LoginController.java
+++ sm-admin/src/main/java/com/salesmanager/admin/controller/login/LoginController.java
@@ -1,10 +1,12 @@
-package com.salesmanager.admin.controller;
+package com.salesmanager.admin.controller.login;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -12,6 +14,13 @@ public class LoginController {
 	@RequestMapping("/login")
 	public String display(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		return "login/login";
+	}
+	
+	@RequestMapping(value="/login.json", method = RequestMethod.POST)
+	public ResponseEntity<String> login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//return "login/login";
+		//retrun RESTEntity
+		return null;
 	}
 
 }
