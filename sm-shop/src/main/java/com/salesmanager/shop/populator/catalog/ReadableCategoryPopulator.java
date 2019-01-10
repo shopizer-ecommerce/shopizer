a27fd6bc15555401a3b50710b8043e6879fcbfd7
earmitage <evans.armitage@gmail.com>
2019-01-10 18:39:01 +0200

    Fix imports

--- sm-shop/src/main/java/com/salesmanager/shop/populator/catalog/ReadableCategoryPopulator.java
+++ sm-shop/src/main/java/com/salesmanager/shop/populator/catalog/ReadableCategoryPopulator.java
@@ -1,7 +1,5 @@
 package com.salesmanager.shop.populator.catalog;
 
-
-
 import org.apache.commons.lang.Validate;
 
 import com.salesmanager.core.business.exception.ConversionException;
@@ -13,69 +11,68 @@ import com.salesmanager.core.model.reference.language.Language;
 import com.salesmanager.shop.model.catalog.category.ReadableCategory;
 
 public class ReadableCategoryPopulator extends
-		AbstractDataPopulator<Category, ReadableCategory> {
+        AbstractDataPopulator<Category, ReadableCategory> {
 
-	@Override
-	public ReadableCategory populate(Category source, ReadableCategory target,
-			MerchantStore store, Language language) throws ConversionException {
-		
-		
-		Validate.notNull(source,"Category must not be null");
-		
-		
-		target.setLineage(source.getLineage());
-		if(source.getDescriptions()!=null && source.getDescriptions().size()>0) {
-			
-			CategoryDescription description = source.getDescription();
-			if(source.getDescriptions().size()>1) {
-				for(CategoryDescription desc : source.getDescriptions()) {
-					if(desc.getLanguage().getCode().equals(language.getCode())) {
-						description = desc;
-						break;
-					}
-				}
-			}
-		
-		
-		
-			if(description!=null) {
-				com.salesmanager.shop.model.catalog.category.CategoryDescription desc = new com.salesmanager.shop.model.catalog.category.CategoryDescription();
-				desc.setFriendlyUrl(description.getSeUrl());
-				desc.setName(description.getName());
-				desc.setId(source.getId());
-				desc.setDescription(description.getName());
-				desc.setKeyWords(description.getMetatagKeywords());
-				desc.setHighlights(description.getCategoryHighlight());
-				desc.setTitle(description.getMetatagTitle());
-				desc.setMetaDescription(description.getMetatagDescription());
-				
-				target.setDescription(desc);
-			}
-		
-		}
-		
-		if(source.getParent()!=null) {
-			com.salesmanager.shop.model.catalog.category.Category parent = new com.salesmanager.shop.model.catalog.category.Category();
-			parent.setCode(source.getParent().getCode());
-			parent.setId(source.getParent().getId());
-			target.setParent(parent);
-		}
-		
-		
-		target.setCode(source.getCode());
-		target.setId(source.getId());
-		target.setDepth(source.getDepth());
-		target.setSortOrder(source.getSortOrder());
-		target.setVisible(source.isVisible());
-		target.setFeatured(source.isFeatured());
+    @Override
+    public ReadableCategory populate(final Category source,
+            final ReadableCategory target,
+            final MerchantStore store,
+            final Language language) throws ConversionException {
 
-		return target;
-		
-	}
+        Validate.notNull(source, "Category must not be null");
 
-	@Override
-	protected ReadableCategory createTarget() {
-		return null;
-	}
+        target.setLineage(source.getLineage());
+        if (source.getDescriptions() != null && source.getDescriptions().size() > 0) {
+
+            CategoryDescription description = source.getDescription();
+            if (source.getDescriptions().size() > 1) {
+                for (final CategoryDescription desc : source.getDescriptions()) {
+                    if (desc.getLanguage().getCode().equals(language.getCode())) {
+                        description = desc;
+                        break;
+                    }
+                }
+            }
+
+            if (description != null) {
+                final com.salesmanager.shop.model.catalog.category.CategoryDescription desc = new com.salesmanager.shop.model.catalog.category.CategoryDescription();
+                desc.setFriendlyUrl(description.getSeUrl());
+                desc.setName(description.getName());
+                desc.setId(source.getId());
+                desc.setDescription(description.getName());
+                desc.setKeyWords(description.getMetatagKeywords());
+                desc.setHighlights(description.getCategoryHighlight());
+                desc.setTitle(description.getMetatagTitle());
+                desc.setMetaDescription(description.getMetatagDescription());
+
+                target.setDescription(desc);
+            }
+
+        }
+
+        if (source.getParent() != null) {
+            final com.salesmanager.shop.model.catalog.category.Category parent = new com.salesmanager.shop.model.catalog.category.Category();
+            parent.setCode(source.getParent().getCode());
+            parent.setId(source.getParent().getId());
+            target.setParent(parent);
+        }
+
+        target.setCode(source.getCode());
+        target.setId(source.getId());
+        if (source.getDepth() != null) {
+            target.setDepth(source.getDepth());
+        }
+        target.setSortOrder(source.getSortOrder());
+        target.setVisible(source.isVisible());
+        target.setFeatured(source.isFeatured());
+
+        return target;
+
+    }
+
+    @Override
+    protected ReadableCategory createTarget() {
+        return null;
+    }
 
 }
