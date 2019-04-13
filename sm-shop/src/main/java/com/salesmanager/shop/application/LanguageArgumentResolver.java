5339f0ddedeb49acfb92630ce1b5160da0b04f8c
Dima <example@example.com>
2019-04-13 18:25:46 +0300

    added resolvers for store and lang

---
+++ sm-shop/src/main/java/com/salesmanager/shop/application/LanguageArgumentResolver.java
@@ -0,0 +1,46 @@
+package com.salesmanager.shop.application;
+
+import com.salesmanager.core.model.merchant.MerchantStore;
+import com.salesmanager.core.model.reference.language.Language;
+import com.salesmanager.shop.utils.LanguageUtils;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LanguageArgumentResolver implements HandlerMethodArgumentResolver {
+
+  @Autowired
+  private LanguageUtils languageUtils;
+  @Autowired
+  private MerchantStoreArgumentResolver merchantStoreArgumentResolver;
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return parameter.getParameterType().equals(Language.class);
+  }
+
+  @Override
+  public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+      NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+    HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+
+    MerchantStore merchantStore = getMerchantStore(parameter, mavContainer, webRequest,
+        binderFactory);
+    return languageUtils.getRESTLanguage(request, merchantStore);
+  }
+
+  private MerchantStore getMerchantStore(MethodParameter parameter,
+      ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+      WebDataBinderFactory binderFactory) throws Exception {
+    return (MerchantStore) merchantStoreArgumentResolver.resolveArgument(parameter, mavContainer, webRequest, binderFactory);
+  }
+}
