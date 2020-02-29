9fdff27ba9b653924d52a70206ed7c3eed3d3df8
shopizer-ecommerce <csamson777@yahoo.com>
2020-02-28 21:36:41 -0500

    Fixed content manag

---
+++ sm-shop/src/main/java/com/salesmanager/shop/utils/CloudFilePathUtils.java
@@ -0,0 +1,66 @@
+package com.salesmanager.shop.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import com.salesmanager.core.model.content.FileContentType;
+import com.salesmanager.core.model.merchant.MerchantStore;
+import com.salesmanager.shop.constants.Constants;
+
+@Component
+public class CloudFilePathUtils extends AbstractimageFilePath {
+
+	private String basePath = Constants.STATIC_URI;
+
+	@Override
+	public String getBasePath() {
+		return basePath;
+	}
+
+	@Override
+	public void setBasePath(String basePath) {
+		this.basePath = basePath;
+	}
+	@Override
+	public String getContextPath() {
+		return super.getProperties().getProperty(CONTEXT_PATH);
+	}
+	
+	/**
+	 * Builds a static content image file path that can be used by image servlet
+	 * utility for getting the physical image
+	 * @param store
+	 * @param imageName
+	 * @return
+	 */
+	@Override
+	public String buildStaticImageUtils(MerchantStore store, String imageName) {
+		StringBuilder imgName = new StringBuilder().append(getBasePath()).append(Constants.FILES_URI).append(Constants.SLASH).append(store.getCode()).append(Constants.SLASH);
+				if(!StringUtils.isBlank(imageName)) {
+					imgName.append(imageName);
+				}
+		return imgName.toString();
+				
+	}
+	
+	/**
+	 * Builds a static content image file path that can be used by image servlet
+	 * utility for getting the physical image by specifying the image type
+	 * @param store
+	 * @param imageName
+	 * @return
+	 */
+	@Override
+	public String buildStaticImageUtils(MerchantStore store, String type, String imageName) {
+		StringBuilder imgName = new StringBuilder().append(getBasePath()).append(Constants.FILES_URI).append(Constants.SLASH).append(store.getCode()).append(Constants.SLASH);
+		if(type!=null && !FileContentType.IMAGE.name().equals(type)) {
+			imgName.append(type).append(Constants.SLASH);
+		}
+		if(!StringUtils.isBlank(imageName)) {
+			imgName.append(imageName);
+		}
+		return imgName.toString();
+
+	}
+
+}
