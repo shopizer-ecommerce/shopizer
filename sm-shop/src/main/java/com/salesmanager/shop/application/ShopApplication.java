f81a1ab7b2133287ec15e53ea5f667584e9a1bdb
shopizer-ecommerce <csamson777@yahoo.com>
2020-03-17 21:50:46 -0400

    Fixed spring boot 2.x startup war

--- sm-shop/src/main/java/com/salesmanager/shop/application/ShopApplication.java
+++ sm-shop/src/main/java/com/salesmanager/shop/application/ShopApplication.java
@@ -3,10 +3,17 @@ package com.salesmanager.shop.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer {
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShopApplication.class);
+    }
 	
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
