90c0e65579f6d19a7a4ca920f42a6e3a4f6c6433
wtune <soundofwtune@outlook.com>
2020-03-19 23:23:14 +0800

    Remove unnecessary distinct on unique column

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/attribute/ProductOptionValueRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/attribute/ProductOptionValueRepository.java
@@ -15,7 +15,7 @@ public interface ProductOptionValueRepository extends JpaRepository<ProductOptio
 	@Query("select p from ProductOptionValue p join fetch p.merchantStore pm left join fetch p.descriptions pd where p.id = ?2  and pm.id = ?1")
 	ProductOptionValue findOne(Integer storeId, Long id);
 	
-	@Query("select distinct p from ProductOptionValue p join fetch p.merchantStore pm left join fetch p.descriptions pd where pm.id = ?1 and pd.language.id = ?2")
+	@Query("select p from ProductOptionValue p join fetch p.merchantStore pm left join fetch p.descriptions pd where pm.id = ?1 and pd.language.id = ?2")
 	List<ProductOptionValue> findByStoreId(Integer storeId, Integer languageId);
 	
 	@Query("select p from ProductOptionValue p join fetch p.merchantStore pm left join fetch p.descriptions pd where pm.id = ?1 and p.code = ?2")
@@ -25,7 +25,7 @@ public interface ProductOptionValueRepository extends JpaRepository<ProductOptio
 	public List<ProductOptionValue> findByName(Integer storeId, String name, Integer languageId);
 	
 	
-	@Query("select distinct p from ProductOptionValue p join fetch p.merchantStore pm left join fetch p.descriptions pd where pm.id = ?1 and pd.language.id = ?2 and p.productOptionDisplayOnly = ?3")
+	@Query("select p from ProductOptionValue p join fetch p.merchantStore pm left join fetch p.descriptions pd where pm.id = ?1 and pd.language.id = ?2 and p.productOptionDisplayOnly = ?3")
 	public List<ProductOptionValue> findByReadOnly(Integer storeId, Integer languageId, boolean readOnly);
 	
 
