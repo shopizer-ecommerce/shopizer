c887da56be8b7d3836eae2dd4eb4398e7d96edba
en1ma <amine_boudraa@outlook.fr>
2020-04-14 18:36:18 +0200

    Fix - Stored XSS via customer product review

--- sm-shop/src/main/java/com/salesmanager/shop/store/controller/customer/CustomerProductReviewController.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/controller/customer/CustomerProductReviewController.java
@@ -34,6 +34,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.apache.commons.lang3.StringEscapeUtils;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -50,28 +51,28 @@ import java.util.Locale;
 @Controller
 @RequestMapping(Constants.SHOP_URI + "/customer")
 public class CustomerProductReviewController extends AbstractController {
-	
+
 	@Inject
 	private ProductService productService;
-	
+
 	@Inject
 	private LanguageService languageService;
-	
+
 	@Inject
 	private PricingService pricingService;
-	
+
 	@Inject
 	private ProductReviewService productReviewService;
-	
+
 	@Inject
 	private CustomerService customerService;
-	
+
 	@Inject
 	private CustomerFacade customerFacade;
-	
+
 	@Inject
 	private LabelUtils messages;
-	
+
 	@Inject
 	@Qualifier("img")
 	private ImageFilePath imageUtils;
@@ -79,24 +80,24 @@ public class CustomerProductReviewController extends AbstractController {
 	@PreAuthorize("hasRole('AUTH_CUSTOMER')")
 	@RequestMapping(value="/review.html", method=RequestMethod.GET)
 	public String displayProductReview(@RequestParam Long productId, Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 
 	    MerchantStore store = getSessionAttribute(Constants.MERCHANT_STORE, request);
 	    Language language = super.getLanguage(request);
 
-        
-        
+
+
         //get product
         Product product = productService.getById(productId);
         if(product==null) {
         	return "redirect:" + Constants.SHOP_URI;
         }
-        
+
         if(product.getMerchantStore().getId().intValue()!=store.getId().intValue()) {
         	return "redirect:" + Constants.SHOP_URI;
         }
-        
-        
+
+
         //create readable product
         ReadableProduct readableProduct = new ReadableProduct();
         ReadableProductPopulator readableProductPopulator = new ReadableProductPopulator();
@@ -104,83 +105,84 @@ public class CustomerProductReviewController extends AbstractController {
         readableProductPopulator.setimageUtils(imageUtils);
         readableProductPopulator.populate(product, readableProduct,  store, language);
         model.addAttribute("product", readableProduct);
-        
+
 
         Customer customer =  customerFacade.getCustomerByUserName(request.getRemoteUser(), store);
-        
+
         List<ProductReview> reviews = productReviewService.getByProduct(product, language);
 	    for(ProductReview r : reviews) {
 	    	if(r.getCustomer().getId().longValue()==customer.getId().longValue()) {
-	    		
+
 				ReadableProductReviewPopulator reviewPopulator = new ReadableProductReviewPopulator();
 				ReadableProductReview rev = new ReadableProductReview();
 				reviewPopulator.populate(r, rev, store, language);
-	    		
+
 	    		model.addAttribute("customerReview", rev);
 	    		break;
 	    	}
 	    }
-        
-        
+
+
         ProductReview review = new ProductReview();
         review.setCustomer(customer);
         review.setProduct(product);
-        
+
         ReadableProductReview productReview = new ReadableProductReview();
         ReadableProductReviewPopulator reviewPopulator = new ReadableProductReviewPopulator();
         reviewPopulator.populate(review,  productReview, store, language);
-        
+
         model.addAttribute("review", productReview);
         model.addAttribute("reviews", reviews);
-		
-		
+
+
 		/** template **/
 		StringBuilder template = new StringBuilder().append(ControllerConstants.Tiles.Customer.review).append(".").append(store.getStoreTemplate());
 
 		return template.toString();
-		
+
 	}
-	
-	
+
+
 	@PreAuthorize("hasRole('AUTH_CUSTOMER')")
 	@RequestMapping(value="/review/submit.html", method=RequestMethod.POST)
 	public String submitProductReview(@ModelAttribute("review") PersistableProductReview review, BindingResult bindingResult, Model model, HttpServletRequest request, HttpServletResponse response, Locale locale) throws Exception {
-		
+
 
 	    MerchantStore store = getSessionAttribute(Constants.MERCHANT_STORE, request);
 	    Language language = getLanguage(request);
-	    
+
         Customer customer =  customerFacade.getCustomerByUserName(request.getRemoteUser(), store);
-        
+
         if(customer==null) {
         	return "redirect:" + Constants.SHOP_URI;
         }
 
-	    
+
 	    Product product = productService.getById(review.getProductId());
 	    if(product==null) {
 	    	return "redirect:" + Constants.SHOP_URI;
 	    }
-	    
+			review.setDescription(StringEscapeUtils.escapeHtml4(review.getDescription()));
+			
 	    if(StringUtils.isBlank(review.getDescription())) {
 	    	FieldError error = new FieldError("description","description",messages.getMessage("NotEmpty.review.description", locale));
 			bindingResult.addError(error);
 	    }
-	    
+
 	    if(review.getRating()==null) {
 	    	FieldError error = new FieldError("rating","rating",messages.getMessage("NotEmpty.review.rating", locale, "Product rating is required"));
 			bindingResult.addError(error);
 	    }
-	    
 
-	    
+
+
         ReadableProduct readableProduct = new ReadableProduct();
         ReadableProductPopulator readableProductPopulator = new ReadableProductPopulator();
         readableProductPopulator.setPricingService(pricingService);
         readableProductPopulator.setimageUtils(imageUtils);
         readableProductPopulator.populate(product, readableProduct,  store, language);
         model.addAttribute("product", readableProduct);
-	    
+
 
 		/** template **/
 		StringBuilder template = new StringBuilder().append(ControllerConstants.Tiles.Customer.review).append(".").append(store.getStoreTemplate());
@@ -191,47 +193,47 @@ public class CustomerProductReviewController extends AbstractController {
             return template.toString();
 
         }
-		
-        
+
+
         //check if customer has already evaluated the product
 	    List<ProductReview> reviews = productReviewService.getByProduct(product);
-	    
+
 	    for(ProductReview r : reviews) {
 	    	if(r.getCustomer().getId().longValue()==customer.getId().longValue()) {
 				ReadableProductReviewPopulator reviewPopulator = new ReadableProductReviewPopulator();
 				ReadableProductReview rev = new ReadableProductReview();
 				reviewPopulator.populate(r, rev, store, language);
-	    		
+
 	    		model.addAttribute("customerReview", rev);
 	    		return template.toString();
 	    	}
 	    }
 
-	    
+
 	    PersistableProductReviewPopulator populator = new PersistableProductReviewPopulator();
 	    populator.setCustomerService(customerService);
 	    populator.setLanguageService(languageService);
 	    populator.setProductService(productService);
-	    
+
 	    review.setDate(DateUtil.formatDate(new Date()));
 	    review.setCustomerId(customer.getId());
-	    
+
 	    ProductReview productReview = populator.populate(review, store, language);
 	    productReviewService.create(productReview);
-        
+
         model.addAttribute("review", review);
         model.addAttribute("success", "success");
-        
+
 		ReadableProductReviewPopulator reviewPopulator = new ReadableProductReviewPopulator();
 		ReadableProductReview rev = new ReadableProductReview();
 		reviewPopulator.populate(productReview, rev, store, language);
-		
+
         model.addAttribute("customerReview", rev);
 
 		return template.toString();
-		
+
 	}
-	
-	
-	
+
+
+
 }
