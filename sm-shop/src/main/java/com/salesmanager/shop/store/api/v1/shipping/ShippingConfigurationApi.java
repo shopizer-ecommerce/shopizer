3b93b9142a006c904b586ea96d40db46d8183649
shopizer-ecommerce <csamson777@yahoo.com>
2020-06-14 21:41:59 -0400

    ShippingOrigin

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/shipping/ShippingConfigurationApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/shipping/ShippingConfigurationApi.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -61,9 +62,8 @@ public class ShippingConfigurationApi {
 	
 	@RequestMapping(value = { "/private/origin" }, method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.OK)
-	@ResponseBody
 	public void saveShippingOrigin(
-			PersistableAddress address,
+			@RequestBody PersistableAddress address,
 			@ApiIgnore MerchantStore merchantStore,
 			@ApiIgnore Language language) {
 
