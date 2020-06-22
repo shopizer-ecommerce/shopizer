b9eaf073f51c082dc72d1ef89502c05e0eed3aa1
syshex <ruizinho>
2020-06-22 11:27:47 +0100

    fix get Customer List exception when no customers exist - api v1

--- sm-core-model/src/main/java/com/salesmanager/core/model/customer/CustomerList.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/customer/CustomerList.java
@@ -1,5 +1,6 @@
 package com.salesmanager.core.model.customer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.salesmanager.core.model.common.EntityList;
@@ -11,7 +12,7 @@ public class CustomerList extends EntityList {
 	 * 
 	 */
 	private static final long serialVersionUID = -3108842276158069739L;
-	private List<Customer> customers;
+	private List<Customer> customers = new ArrayList<>();
 	public void setCustomers(List<Customer> customers) {
 		this.customers = customers;
 	}
