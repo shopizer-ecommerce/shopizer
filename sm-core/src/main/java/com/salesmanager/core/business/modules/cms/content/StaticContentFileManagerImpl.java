e157bd24cf6d4eaad027b77b14e2ea169c7b77e0
Diego Carbajal <diegodcr@gmail.com>
2020-07-22 17:53:11 -0500

    gcp storage for static content

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/content/StaticContentFileManagerImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/content/StaticContentFileManagerImpl.java
@@ -6,10 +6,10 @@ package com.salesmanager.core.business.modules.cms.content;
 import java.util.List;
 import com.salesmanager.core.business.exception.ServiceException;
 import com.salesmanager.core.business.modules.cms.content.infinispan.CmsStaticContentFileManagerImpl;
+import com.salesmanager.core.business.modules.cms.impl.CMSManager;
 import com.salesmanager.core.model.content.FileContentType;
 import com.salesmanager.core.model.content.InputContentFile;
 import com.salesmanager.core.model.content.OutputContentFile;
-
 
 /**
  * @author Umesh Awasthi
@@ -25,8 +25,6 @@ public class StaticContentFileManagerImpl extends StaticContentFileManager {
   private FileGet getFile;
   private FileRemove removeFile;
 
-
-
   @Override
   public void addFile(final String merchantStoreCode, final InputContentFile inputStaticContentData)
       throws ServiceException {
@@ -35,38 +33,36 @@ public class StaticContentFileManagerImpl extends StaticContentFileManager {
   }
 
   /**
-   * Implementation for add static data files. This method will called respected add files method of
-   * underlying CMSStaticContentManager. For CMS Content files
-   * {@link CmsStaticContentFileManagerImpl} will take care of adding given content images with
-   * Infinispan cache.
+   * Implementation for add static data files. This method will called respected
+   * add files method of underlying CMSStaticContentManager. For CMS Content files
+   * {@link CmsStaticContentFileManagerImpl} will take care of adding given
+   * content images with Infinispan cache.
    * 
-   * @param merchantStoreCode merchant store.
+   * @param merchantStoreCode          merchant store.
    * @param inputStaticContentDataList Input content images
    * @throws ServiceException
    */
   @Override
-  public void addFiles(final String merchantStoreCode,
-      final List<InputContentFile> inputStaticContentDataList) throws ServiceException {
+  public void addFiles(final String merchantStoreCode, final List<InputContentFile> inputStaticContentDataList)
+      throws ServiceException {
     uploadFile.addFiles(merchantStoreCode, inputStaticContentDataList);
   }
 
   @Override
-  public void removeFile(final String merchantStoreCode, final FileContentType staticContentType,
-      final String fileName) throws ServiceException {
+  public void removeFile(final String merchantStoreCode, final FileContentType staticContentType, final String fileName)
+      throws ServiceException {
     removeFile.removeFile(merchantStoreCode, staticContentType, fileName);
 
   }
 
-
   @Override
-  public OutputContentFile getFile(String merchantStoreCode, FileContentType fileContentType,
-      String contentName) throws ServiceException {
+  public OutputContentFile getFile(String merchantStoreCode, FileContentType fileContentType, String contentName)
+      throws ServiceException {
     return getFile.getFile(merchantStoreCode, fileContentType, contentName);
   }
 
   @Override
-  public List<String> getFileNames(String merchantStoreCode, FileContentType fileContentType)
-      throws ServiceException {
+  public List<String> getFileNames(String merchantStoreCode, FileContentType fileContentType) throws ServiceException {
     return getFile.getFileNames(merchantStoreCode, fileContentType);
   }
 
@@ -80,8 +76,6 @@ public class StaticContentFileManagerImpl extends StaticContentFileManager {
   public void removeFiles(String merchantStoreCode) throws ServiceException {
     removeFile.removeFiles(merchantStoreCode);
   }
-
-
 
   public void setRemoveFile(FileRemove removeFile) {
     this.removeFile = removeFile;
@@ -107,5 +101,9 @@ public class StaticContentFileManagerImpl extends StaticContentFileManager {
     return uploadFile;
   }
 
+  @Override
+  public CMSManager getCmsManager() {
+    return null;
+  }
 
 }
