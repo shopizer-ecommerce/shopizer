7e24c0e385984b5be62e711cae3cc99793dc4489
Ale Feltes <afeltes@sodep.com.py>
2020-12-09 18:06:12 -0300

    Adds locale based bigdecimal parser

--- sm-shop/src/main/java/com/salesmanager/shop/utils/MerchantUtils.java
+++ sm-shop/src/main/java/com/salesmanager/shop/utils/MerchantUtils.java
@@ -1,6 +1,11 @@
 package com.salesmanager.shop.utils;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Date;
+import java.util.Locale;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -23,4 +28,19 @@ public class MerchantUtils {
 		return null;
 	}
 
+	/**
+	 * Locale based bigdecimal parser
+	 * @return
+	 */
+	public static BigDecimal getBigDecimal(String bigDecimal) throws ParseException {
+		NumberFormat decimalFormat = NumberFormat.getInstance(Locale.getDefault());
+		BigDecimal value;
+		if(decimalFormat instanceof DecimalFormat) {
+			((DecimalFormat) decimalFormat).setParseBigDecimal(true);
+			value = (BigDecimal) decimalFormat.parse(bigDecimal);
+		} else {
+			value = new BigDecimal(bigDecimal);
+		}
+		return value;
+	}
 }
