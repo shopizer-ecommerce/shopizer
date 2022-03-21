154bfcb892e0004b9dba002454e47ca52817f787
shopizerecomm <csamson777@yahoo.com>
2022-03-21 19:01:12 -0400

    version 3.0.1

--- sm-shop/src/main/java/com/salesmanager/shop/store/controller/system/MerchantConfigurationFacadeImpl.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/controller/system/MerchantConfigurationFacadeImpl.java
@@ -4,15 +4,17 @@ import static com.salesmanager.shop.constants.Constants.KEY_FACEBOOK_PAGE_URL;
 import static com.salesmanager.shop.constants.Constants.KEY_GOOGLE_ANALYTICS_URL;
 import static com.salesmanager.shop.constants.Constants.KEY_INSTAGRAM_URL;
 import static com.salesmanager.shop.constants.Constants.KEY_PINTEREST_PAGE_URL;
+
 import java.util.Optional;
+
 import javax.inject.Inject;
 
 import org.apache.commons.lang3.StringUtils;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import com.salesmanager.core.business.exception.ServiceException;
 import com.salesmanager.core.business.services.system.MerchantConfigurationService;
 import com.salesmanager.core.model.merchant.MerchantStore;
@@ -21,7 +23,6 @@ import com.salesmanager.core.model.system.MerchantConfig;
 import com.salesmanager.core.model.system.MerchantConfiguration;
 import com.salesmanager.shop.model.system.Configs;
 import com.salesmanager.shop.store.api.exception.ServiceRuntimeException;
-import com.salesmanager.shop.store.controller.order.ShoppingOrderController;
 
 
 @Service
