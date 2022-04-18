ac3e2637291981587fb648e2d4f413edc3b47cd5
arhalai <arhalaitaoufik1@gmail.com>
2022-04-18 16:22:37 +0200

    fix remove false condition

--- sm-core/src/main/java/com/salesmanager/core/business/utils/CatalogServiceHelper.java
+++ sm-core/src/main/java/com/salesmanager/core/business/utils/CatalogServiceHelper.java
@@ -14,60 +14,57 @@ import java.util.stream.Collectors;
 
 public class CatalogServiceHelper {
 
-	/**
-	 * Filters descriptions and set the appropriate language
-	 *
-	 * @param p
-	 * @param language
-	 */
-	public static void setToLanguage(Product p, int language) {
+    /**
+     * Filters descriptions and set the appropriate language
+     *
+     * @param p
+     * @param language
+     */
+    public static void setToLanguage(Product p, int language) {
 
 
-		Set<ProductAttribute> attributes = p.getAttributes();
-		if (attributes != null) {
+        Set<ProductAttribute> attributes = p.getAttributes();
+        if (attributes != null) {
 
-			for (ProductAttribute attribute : attributes) {
+            for (ProductAttribute attribute : attributes) {
 
-				ProductOption po = attribute.getProductOption();
-				if (po.getDescriptions() != null) {
-					Set<ProductOptionDescription> podDescriptions = po.getDescriptions().stream().filter(pod -> pod.getLanguage().getId() == language).collect(Collectors.toSet());
-					po.setDescriptions(podDescriptions);
-				}
+                ProductOption po = attribute.getProductOption();
+                if (po.getDescriptions() != null) {
+                    Set<ProductOptionDescription> podDescriptions = po.getDescriptions().stream().filter(pod -> pod.getLanguage().getId() == language).collect(Collectors.toSet());
+                    po.setDescriptions(podDescriptions);
+                }
 
-				ProductOptionValue pov = attribute.getProductOptionValue();
-				if (pov.getDescriptions() != null) {
-					Set<ProductOptionValueDescription> povdDescriptions = pov.getDescriptions().stream().filter(pod -> pod.getLanguage().getId() == language).collect(Collectors.toSet());
-					pov.setDescriptions(povdDescriptions);
-				}
-			}
-		}
+                ProductOptionValue pov = attribute.getProductOptionValue();
+                if (pov.getDescriptions() != null) {
+                    Set<ProductOptionValueDescription> povdDescriptions = pov.getDescriptions().stream().filter(pod -> pod.getLanguage().getId() == language).collect(Collectors.toSet());
+                    pov.setDescriptions(povdDescriptions);
+                }
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * Overwrites the availability in order to return 1 price / region
-	 *
-	 * @param product
-	 * @param locale
-	 */
-	public static void setToAvailability(Product product, Locale locale) {
+    /**
+     * Overwrites the availability in order to return 1 price / region
+     *
+     * @param product
+     * @param locale
+     */
+    public static void setToAvailability(Product product, Locale locale) {
 
-		Set<ProductAvailability> availabilities = product.getAvailabilities();
-		Set<ProductAvailability> productAvailabilities = new HashSet<ProductAvailability>();
+        Set<ProductAvailability> availabilities = product.getAvailabilities();
+        Set<ProductAvailability> productAvailabilities = new HashSet<ProductAvailability>();
 
-		Optional<ProductAvailability> defaultAvailability = availabilities.stream().filter(productAvailability -> productAvailability.getRegion().equals(Constants.ALL_REGIONS)).findFirst();
-		Optional<ProductAvailability> localeAvailability = availabilities.stream().filter(productAvailability -> productAvailability.getRegion().equals(locale.getCountry())).findFirst();
-		if (defaultAvailability.isPresent()) {
-			productAvailabilities.add(defaultAvailability.get());
-		}
-		if (localeAvailability.isPresent()) {
-			productAvailabilities.add(localeAvailability.get());
-		}
+        Optional<ProductAvailability> defaultAvailability = availabilities.stream().filter(productAvailability -> productAvailability.getRegion().equals(Constants.ALL_REGIONS)).findFirst();
+        Optional<ProductAvailability> localeAvailability = availabilities.stream().filter(productAvailability -> productAvailability.getRegion().equals(locale.getCountry())).findFirst();
+        if (defaultAvailability.isPresent()) {
+            productAvailabilities.add(defaultAvailability.get());
+        }
+        if (localeAvailability.isPresent()) {
+            productAvailabilities.add(localeAvailability.get());
+        }
+        product.setAvailabilities(productAvailabilities);
 
-		if (productAvailabilities.isEmpty()) {
-			product.setAvailabilities(productAvailabilities);
-
-		}
-	}
+    }
 
 }
