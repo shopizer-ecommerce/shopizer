2ae8876998ef8ddb701973acfe35cd12a0139d0c
mahesh <maheshryali@gmail.com>
2022-10-16 12:21:49 +0530

    changesnew

---
+++ jenkinsfile.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent {label ('shopizer')}
+    parameters {
+         parameters { choice(name: 'branch', choices: ['master', 'developbranch'], description: 'forselectingbranch')
+                      choice(name: 'maven', choices: ['package', 'install', 'clean package', 'clean install'], description: 'formavenbuildpackage') 
+                      }
+    }                  
+    stages {
+        stage('vcs') {
+            steps {
+                git branch: ${params.branch},
+                       url: 'https://github.com/maheshryali/shopizer.git'
+            }
+        }
+        stage('maven build') {
+            steps {
+                sh ('mvn ${params.maven}')
+                }
+        }
+    }
+     triggers { pollSCM('* * * * *') }
+}
