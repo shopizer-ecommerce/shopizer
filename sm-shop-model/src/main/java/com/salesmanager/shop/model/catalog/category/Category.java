6b822cb233328dbdf2b694d40919b62f419ad656
shopizerecomm <csamson777@yahoo.com>
2023-01-17 19:47:20 -0500

    ProductGroup fix

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/category/Category.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/category/Category.java
@@ -12,11 +12,18 @@ public class Category extends Entity implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private String code;
+	private CategoryDescription description;
 	public String getCode() {
 		return code;
 	}
 	public void setCode(String code) {
 		this.code = code;
+	}
+	public CategoryDescription getDescription() {
+		return description;
+	}
+	public void setDescription(CategoryDescription description) {
+		this.description = description;
 	}
 	
 }
--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/product/ProductGroupApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/product/ProductGroupApi.java
@@ -169,7 +169,7 @@ public class ProductGroupApi {
 	Product product = null;
     try {
       // get the product
-    	product = productService.getById(productId);
+    	product = productService.findOne(productId, merchantStore);
 
       if (product == null) {
         response.sendError(404, "Product not fount for id " + productId);
