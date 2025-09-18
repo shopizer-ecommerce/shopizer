cafbe84504ceb3a1b19812c68f63024c6dcdbc32
kuntal1461 <kuntal.1461@gmail.com>
2025-09-18 14:31:32 +0530

    By Kuntal: Fix constructor dependency injection in ManufacturerServiceImpl to pass repository to SalesManagerEntityServiceImpl

--- sm-core/src/main/java/com/salesmanager/core/business/services/catalog/product/manufacturer/ManufacturerServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/services/catalog/product/manufacturer/ManufacturerServiceImpl.java
@@ -1,6 +1,5 @@
 package com.salesmanager.core.business.services.catalog.product.manufacturer;
 
-
 import com.salesmanager.core.business.exception.ServiceException;
 import com.salesmanager.core.business.repositories.catalog.product.manufacturer.ManufacturerRepository;
 import com.salesmanager.core.business.repositories.catalog.product.manufacturer.PageableManufacturerRepository;
@@ -10,37 +9,31 @@ import com.salesmanager.core.model.catalog.product.manufacturer.Manufacturer;
 import com.salesmanager.core.model.catalog.product.manufacturer.ManufacturerDescription;
 import com.salesmanager.core.model.merchant.MerchantStore;
 import com.salesmanager.core.model.reference.language.Language;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.helper.Validate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.inject.Inject;
 import java.util.HashSet;
 import java.util.List;
 
-
-
 @Service("manufacturerService")
+@Slf4j
 public class ManufacturerServiceImpl extends SalesManagerEntityServiceImpl<Long, Manufacturer>
     implements ManufacturerService {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(ManufacturerServiceImpl.class);
+  private final PageableManufacturerRepository pageableManufacturerRepository;
+  private final ManufacturerRepository manufacturerRepository;
 
-  @Inject
-  private PageableManufacturerRepository pageableManufacturerRepository;
-  
-  private ManufacturerRepository manufacturerRepository;
-
-  @Inject
-  public ManufacturerServiceImpl(ManufacturerRepository manufacturerRepository) {
-    super(manufacturerRepository);
+  public ManufacturerServiceImpl(
+      PageableManufacturerRepository pageableManufacturerRepository,
+      ManufacturerRepository manufacturerRepository) {
+    super(manufacturerRepository); 
+    this.pageableManufacturerRepository = pageableManufacturerRepository;
     this.manufacturerRepository = manufacturerRepository;
   }
-
   @Override
   public void delete(Manufacturer manufacturer) throws ServiceException {
     manufacturer = this.getById(manufacturer.getId());
@@ -50,9 +43,7 @@ public class ManufacturerServiceImpl extends SalesManagerEntityServiceImpl<Long,
   @Override
   public Long getCountManufAttachedProducts(Manufacturer manufacturer) throws ServiceException {
     return manufacturerRepository.countByProduct(manufacturer.getId());
-    // .getCountManufAttachedProducts( manufacturer );
   }
-
 
   @Override
   public List<Manufacturer> listByStore(MerchantStore store, Language language)
@@ -74,12 +65,9 @@ public class ManufacturerServiceImpl extends SalesManagerEntityServiceImpl<Long,
   @Override
   public void addManufacturerDescription(Manufacturer manufacturer,
       ManufacturerDescription description) throws ServiceException {
-
-
     if (manufacturer.getDescriptions() == null) {
-      manufacturer.setDescriptions(new HashSet<ManufacturerDescription>());
+      manufacturer.setDescriptions(new HashSet<>());
     }
-
     manufacturer.getDescriptions().add(description);
     description.setManufacturer(manufacturer);
     update(manufacturer);
@@ -87,24 +75,19 @@ public class ManufacturerServiceImpl extends SalesManagerEntityServiceImpl<Long,
 
   @Override
   public void saveOrUpdate(Manufacturer manufacturer) throws ServiceException {
-
-    LOGGER.debug("Creating Manufacturer");
-
+    log.debug("Saving manufacturer");
     if (manufacturer.getId() != null && manufacturer.getId() > 0) {
       super.update(manufacturer);
-
     } else {
       super.create(manufacturer);
-
     }
   }
 
   @Override
-  public Manufacturer getByCode(com.salesmanager.core.model.merchant.MerchantStore store,
-      String code) {
+  public Manufacturer getByCode(MerchantStore store, String code) {
     return manufacturerRepository.findByCodeAndMerchandStore(code, store.getId());
   }
-  
+
   @Override
   public Manufacturer getById(Long id) {
     return manufacturerRepository.findOne(id);
@@ -114,15 +97,15 @@ public class ManufacturerServiceImpl extends SalesManagerEntityServiceImpl<Long,
   public List<Manufacturer> listByProductsInCategory(MerchantStore store, Category category,
       Language language) throws ServiceException {
     Validate.notNull(store, "Store cannot be null");
-    Validate.notNull(category,"Category cannot be null");
+    Validate.notNull(category, "Category cannot be null");
     Validate.notNull(language, "Language cannot be null");
-    return manufacturerRepository.findByProductInCategoryId(store.getId(), category.getLineage(), language.getId());
+    return manufacturerRepository.findByProductInCategoryId(
+        store.getId(), category.getLineage(), language.getId());
   }
 
   @Override
   public Page<Manufacturer> listByStore(MerchantStore store, Language language, int page, int count)
       throws ServiceException {
-
     Pageable pageRequest = PageRequest.of(page, count);
     return pageableManufacturerRepository.findByStore(store.getId(), language.getId(), null, pageRequest);
   }
@@ -136,7 +119,6 @@ public class ManufacturerServiceImpl extends SalesManagerEntityServiceImpl<Long,
   @Override
   public Page<Manufacturer> listByStore(MerchantStore store, Language language, String name,
       int page, int count) throws ServiceException {
-
     Pageable pageRequest = PageRequest.of(page, count);
     return pageableManufacturerRepository.findByStore(store.getId(), language.getId(), name, pageRequest);
   }
@@ -144,7 +126,6 @@ public class ManufacturerServiceImpl extends SalesManagerEntityServiceImpl<Long,
   @Override
   public Page<Manufacturer> listByStore(MerchantStore store, String name, int page, int count)
       throws ServiceException {
-
     Pageable pageRequest = PageRequest.of(page, count);
     return pageableManufacturerRepository.findByStore(store.getId(), name, pageRequest);
   }
